2e14c802fbe18ce0f89738aac82a3bd66fef028b
Mayuresh <mayureshkn99@gmail.com>
2024-04-15 20:17:53 -0400

    feat: add public subnets

--- Network/main.tf
+++ Network/main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_vpc" "dev_vpc" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
@@ -15,5 +19,15 @@ resource "aws_internet_gateway" "igw_dev" {
 
   tags = {
     "Name" = "igw"
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+  count             = length(var.public_cidr_blocks)
+  vpc_id            = aws_vpc.dev_vpc.id
+  cidr_block        = var.public_cidr_blocks[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = {
+    "Name" = "Public-Subnet-${count.index+1}"
   }
 }
--- Network/variables.tf
+++ Network/variables.tf
@@ -3,3 +3,9 @@ variable "vpc_cidr" {
   type        = string
   description = "VPC to host static web site"
 }
+
+variable "public_cidr_blocks" {
+  default     = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24", "10.1.4.0/24"]
+  type        = list(string)
+  description = "Public Subnet CIDRs"
+}
