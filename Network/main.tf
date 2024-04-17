ac2bf699940a98de2267650a69a0d5ad192a375a
Mayuresh <mayureshkn99@gmail.com>
2024-04-17 01:13:18 -0400

    feat: add NAT Gateway

--- Network/main.tf
+++ Network/main.tf
@@ -58,3 +58,34 @@ resource "aws_route_table_association" "public_routes" {
   route_table_id = aws_route_table.public_rt.id
   subnet_id      = aws_subnet.public_subnets[count.index].id
 }
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnets[0].id
+
+  tags = {
+    Name = "NAT GW"
+  }
+}
+
+resource "aws_route_table" "nat_rt" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "route-nat-gw"
+  }
+}
+
+resource "aws_route_table_association" "nat_routes" {
+  subnet_id      = aws_subnet.private_subnets[0].id
+  route_table_id = aws_route_table.nat_rt.id
+}
